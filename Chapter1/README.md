528d55c4ab0f3353991c39c0ba456ea779035e16
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-14 05:03:37 +0000

    ✒ add ex2

---
+++ Chapter1/README.md
@@ -0,0 +1,4 @@
+# Chapter 1
+## The Way of the Program
+
+[![Replit](https://img.shields.io/badge/Replit.com-1D2021?style=for-the-badge&logo=replit&logoColor=white)](https://replit.com/@yashpawar/ThinkJuliaExercisesjl#Chapter1/README.md)
---
+++ Chapter1/ex2.jl
@@ -0,0 +1,15 @@
+## Exercise 1-2
+## Start the Julia REPL and use it as a calculator.
+
+## 1. How many seconds are there in 42 minutes 42 seconds?
+println("Ans 1:")
+
+## 2. How many miles are there in 10 kilometers?
+## TIP
+## There are 1.61 kilometers in a mile.
+println("Ans 2:")
+
+## If you run a 10-kilometer race in 37 minutes 48 seconds, what is your average pace (time per mile in minutes and seconds)? What is your average speed in miles per hour?
+println("Ans 3:")
+
+println("End.")
