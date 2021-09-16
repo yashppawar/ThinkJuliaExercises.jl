f691609161d6d2a4498fae4c6bdbd7376d80f574
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-16 13:02:30 +0000

    🎁 complete ch3/ex3

--- Chapter3/ex3.jl
+++ Chapter3/ex3.jl
@@ -16,22 +16,44 @@
 
 ## 1. Type this example into a script and test it.
 println("Ans 1:")
+function dotwice(func)
+    func()
+    func()
+end
 
+function printspam()
+    println("Spam")
+end
+
+dotwice(printspam)
 
 ## 2. Modify dotwice so that it takes two arguments, a function object and a value, and calls the function twice, passing the value as an argument.
 println("Ans 2:")
+function dotwice(func, arg)
+    func(arg)
+    func(arg)
+end
 
+dotwice(println, "dotwice")
 
 ## 3. Copy the definition of printtwice from earlier in this chapter to your script.
 println("Ans 3:")
-
+function printtwice(bruce)
+    println(bruce)
+    println(bruce)
+end 
 
 ## 4. Use the modified version of dotwice to call printtwice twice, passing "spam" as an argument.
 println("Ans 4:")
-
+dotwice(printtwice, "spam")
 
 ## 5. Define a new function called dofour that takes a function object and a value and calls the function four times, passing the value as a parameter. There should be only two statements in the body of this function, not four.
 println("Ans 5:")
+function dofour(func, arg)
+    dotwice(func, arg)
+    dotwice(func, arg)
+end
 
+dofour(printtwice, "spam")
 
 println("End.")
