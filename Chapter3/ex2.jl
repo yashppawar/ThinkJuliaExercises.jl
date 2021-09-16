620fa28f83b87921cea4551b7926993ec8190480
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-16 12:33:36 +0000

    🎁 complete ch3/ex2

--- Chapter3/ex2.jl
+++ Chapter3/ex2.jl
@@ -2,4 +2,10 @@
 ## Write a function named rightjustify that takes a string named s as a parameter and prints the string with enough leading spaces so that the last letter of the string is in column 70 of the display.
 println("Ans.")
 
+function rightjustify(string::String)
+    return ' ' ^ (70 - length(string)) * string
+end 
+
+println(rightjustify("Monty"))
+
 println("End.")
