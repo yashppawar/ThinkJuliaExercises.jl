cea51ee7ecfeb98db1e2ed08060ec7d4611bad15
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-17 15:03:21 +0000

    ➕ add ch4/ex1

---
+++ Chapter4/ex1.jl
@@ -0,0 +1,10 @@
+## Exercise 4-1
+
+## 🐢 = Turtle()
+## @svg begin
+##     forward(🐢, 100)
+##     turn(🐢, -90)
+##     forward(🐢, 100)
+## end
+
+## Now modify the macro to draw a square. Don’t go on until you’ve got it working!
