047af17e0ce79b9e45b9f1c01b63fd32d1dceb4f
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-17 15:10:04 +0000

    ➕ add ch4/ex5

---
+++ Chapter4/ex5.jl
@@ -0,0 +1,5 @@
+## Exercise 4-5
+## Make a copy of square and change the name to polygon. Add another parameter named n and modify the body so it draws an n-sided regular polygon.
+
+## TIP
+## The exterior angles of an n-sided regular polygon are 360n degrees.
