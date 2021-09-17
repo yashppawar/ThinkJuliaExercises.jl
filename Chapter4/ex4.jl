c0b02e98ea8d4d66f88e319d261220bc46c43183
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-17 15:08:20 +0000

    ➕ add ch4/ex4

---
+++ Chapter4/ex4.jl
@@ -0,0 +1,2 @@
+## Exercise 4-4
+## Add another parameter, named len, to square. Modify the body so length of the sides is len, and then modify the function call to provide a second argument. Run the macro again. Test with a range of values for len.
