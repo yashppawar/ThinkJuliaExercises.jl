ae27bfa7c767ea644ebf5583763fe9de102cbbaa
Yash Pawar <yashpawarp@gmail.com>
2021-09-18 17:44:17 +0530

    🎁 complete ch4/ex3

--- Chapter4/ex3.jl
+++ Chapter4/ex3.jl
@@ -1,5 +1,25 @@
 ## Exercise 4-3
 ## Write a function call that passes t as an argument to square, and then run the macro again.
+using ThinkJulia
+
 println("Ans: ")
+
+function square(turtle::Turtle, distance::Int = 100)
+    @svg begin
+        for i in 1:3
+            forward(turtle, distance)
+            turn(turtle, -90)
+        end
+        
+        forward(turtle, distance)
+    end
+end
+
+function drawSquare()
+    turtle = Turtle()
+    square(turtle)
+end
+
+drawSquare()
 
 println("End.")
