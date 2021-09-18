dcd9efa42c2a64f46b84d222152364f18fc21c39
Yash Pawar <yashpawarp@gmail.com>
2021-09-18 19:36:24 +0530

    ➕ add ex6 & 7

---
+++ Chapter4/ex6.jl
@@ -0,0 +1,8 @@
+## Exercise 4-6
+## Write a function called circle that takes a turtle, t, and radius, r, as parameters and that draws an approximate circle by calling polygon with an appropriate length and number of sides. Test your function with a range of values of r.
+
+## TIP
+## Figure out the circumference of the circle and make sure that len * n == circumference.
+println("Ans: ")
+
+println("End.")
---
+++ Chapter4/ex7.jl
@@ -0,0 +1,5 @@
+## Exercise 4-7
+## Make a more general version of circle called arc that takes an additional parameter angle, which determines what fraction of a circle to draw. angle is in units of degrees, so when angle = 360, arc should draw a complete circle.
+println("Ans: ")
+
+println("End.")
