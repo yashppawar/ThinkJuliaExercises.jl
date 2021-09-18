534e7ae9768d08108e79dac01c8cc240c6912222
Yash Pawar <yashpawarp@gmail.com>
2021-09-18 17:39:30 +0530

    🎁 complete ch4/ex2

--- Chapter4/ex2.jl
+++ Chapter4/ex2.jl
@@ -1,5 +1,21 @@
 ## Exercise 4-2
 ## Write a function called square that takes a parameter named t, which is a turtle. It should use the turtle to draw a square.
+using ThinkJulia
+
 println("Ans: ")
+
+function square(turtle::Turtle, distance::Int = 100)
+    @svg begin
+        for i in 1:3
+            forward(turtle, distance)
+            turn(turtle, -90)
+        end
+        
+        forward(turtle, distance)
+    end
+end
+
+turtle = Turtle()
+square(turtle)
 
 println("End.")
