6e60d1110a8a6933c66c5cdfae99af0dcec6e674
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-18 11:25:45 +0000

    🎁 complete ch3/ex4

--- Chapter3/ex4.jl
+++ Chapter3/ex4.jl
@@ -15,7 +15,28 @@
 ## + - - - - + - - - - +
 println("Ans 1:")
 
+function printhline(col::Int = 2)
+    println("+ - - - - " ^ col * "+")
+end
 
+function printbox(col::Int = 2)
+    row = "|         " ^ col * "|"
+    # use loop for iteration
+    for i in 1:4
+        println(row)
+    end
+end
+
+function printgrid(side::Int = 2)
+    for i in 1:side
+        printhline(side)
+        printbox(side)
+    end
+
+    printhline(side)
+end
+
+printgrid()
 ## Write a function that draws a similar grid with four rows and four columns.
 println("Ans 2:")
 
