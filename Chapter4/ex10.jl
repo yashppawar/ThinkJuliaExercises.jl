33128c320d45fc0bb1f99c9ec5211608e84eca8e
Yash Pawar <yashpawarp@gmail.com>
2021-09-19 22:23:40 +0530

    ➕ add ch4/ex10

---
+++ Chapter4/ex10.jl
@@ -0,0 +1,6 @@
+## Exercise 4-10
+## Write an appropriately general set of functions that can draw shapes as in Turtle pies.
+## https://benlauwens.github.io/ThinkJulia.jl/latest/book.html#fig04-3
+println("Ans: ")
+
+println("End.")
