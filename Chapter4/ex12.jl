5f09bea9bf24b4df328fd85485a7c9242004d4ad
Yash Pawar <yashpawarp@gmail.com>
2021-09-19 22:27:43 +0530

    ➕ add ch4/ex12

---
+++ Chapter4/ex12.jl
@@ -0,0 +1,6 @@
+## Exercise 4-12
+## Read about spirals at https://en.wikipedia.org/wiki/Spiral; then write a program that draws an Archimedan spiral as in Archimedan spiral.
+## https://benlauwens.github.io/ThinkJulia.jl/latest/book.html#fig04-4
+println("Ans : ")
+
+println("End.")
