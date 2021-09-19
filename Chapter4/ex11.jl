1f01333e45d7e9d1447786efe8eb5dccbf3765be
Yash Pawar <yashpawarp@gmail.com>
2021-09-19 22:24:50 +0530

    ➕ add ch4/ex11

---
+++ Chapter4/ex11.jl
@@ -0,0 +1,7 @@
+## Exercise 4-11
+## The letters of the alphabet can be constructed from a moderate number of basic elements, like vertical and horizontal lines and a few curves. Design an alphabet that can be drawn with a minimal number of basic elements and then write functions that draw the letters.
+
+## You should write one function for each letter, with names draw_a, draw_b, etc., and put your functions in a file named letters.jl.
+println("Ans: ")
+
+println("End.")
