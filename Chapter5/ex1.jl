d0eb6849dd7978cef18a1ecdc0b1c70f22b8365c
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-21 16:16:10 +0000

    🎁 complete ch5/ex1

--- Chapter5/ex1.jl
+++ Chapter5/ex1.jl
@@ -2,4 +2,23 @@
 ## As an exercise, draw a stack diagram for printn called with s = "Hello" and n = 2. Then write a function called do_n that takes a function object and a number, n, as arguments, and that calls the given function n times.
 println("Ans: ")
 
+println(" n --> 2")
+println(" n --> 1")
+println(" n --> 0")
+
+function printhello()
+    println("Hello")
+end 
+
+function do_n(func, n)
+    if n <= 0
+        return
+    end
+
+    func()
+    do_n(func, n - 1)
+end 
+
+do_n(printhello, 3)
+
 println("End.")
