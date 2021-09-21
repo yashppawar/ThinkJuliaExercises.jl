41b09ee898516a52eadf33b2d9c97f45acabf35c
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-21 15:47:27 +0000

    ➕ add ch5/ex1

---
+++ Chapter5/ex1.jl
@@ -0,0 +1,5 @@
+## Exercise 5-1
+## As an exercise, draw a stack diagram for printn called with s = "Hello" and n = 2. Then write a function called do_n that takes a function object and a number, n, as arguments, and that calls the given function n times.
+println("Ans: ")
+
+println("End.")
