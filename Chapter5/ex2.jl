550a42ce3984011dcd43fb05bb8115d585f99d38
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-22 13:38:10 +0000

    🎁 complete ch5/ex2

--- Chapter5/ex2.jl
+++ Chapter5/ex2.jl
@@ -6,5 +6,17 @@
 
 ## Write a script that reads the current time and converts it to a time of day in hours, minutes, and seconds, plus the number of days since the epoch.
 println("Ans: ")
+using Printf
+
+curr_time = time()
+
+seconds = curr_time % 60
+h = 60 * 60
+minutes = ((curr_time - seconds) % (h))/h
+d = 60 * 60 * 24
+hours = ((curr_time - (seconds + minutes)) % (d)) / d
+days = (curr_time - (seconds + minutes + hours)) / (60 * 60 * 24)
+
+@printf("%d Days, %d Hours, %d Minutes, %d Seconds\n", days, hours, minutes, seconds)
 
 println("End.")
