356a52526972254c681a77f1a3ab2c752034ea72
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-22 13:43:41 +0000

    ➕ add ch5/ex3

---
+++ Chapter5/ex3.jl
@@ -0,0 +1,12 @@
+## Exercise 5-3
+## Fermat’s Last Theorem says that there are no positive integers a, b, and c such that
+
+## a^n+b^n=c^n
+## for any value of n greater than 2.
+
+## Write a function named checkfermat that takes four parameters—a, b, c and n—and checks to see if Fermat’s theorem holds. If n is greater than 2 and a^n + b^n == c^n the program should print, “Holy smokes, Fermat was wrong!” Otherwise the program should print, “No, that doesn’t work.”
+
+## Write a function that prompts the user to input values for a, b, c and n, converts them to integers, and uses checkfermat to check whether they violate Fermat’s theorem.
+println("Ans: ")
+
+println("End.")
