69bab58b407e36c4b89de8c993cd0123ba70b5da
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-22 13:23:02 +0000

    ➕ add ch5/ex2

---
+++ Chapter5/ex2.jl
@@ -0,0 +1,10 @@
+## Exercise 5-2
+## The function time returns the current Greenwich Mean Time in seconds since “the epoch”, which is an arbitrary time used as a reference point. On UNIX systems, the epoch is 1 January 1970.
+
+## julia> time()
+## 1.554236060271472e9
+
+## Write a script that reads the current time and converts it to a time of day in hours, minutes, and seconds, plus the number of days since the epoch.
+println("Ans: ")
+
+println("End.")
