1e44e523272ea7d7b57f2b692a72c3936cdde2cc
Yash Pawar <yashpawarp@gmail.com>
2021-09-23 15:36:38 +0530

    🎁 complete ch5/ex6

--- Chapter5/ex6.jl
+++ Chapter5/ex6.jl
@@ -1,6 +1,7 @@
 ## The following exercises use the ThinkJulia module, described in Case Study: Interface Design:
 ## Exercise 5-6
 ## Read the following function and see if you can figure out what it does (see the examples in Case Study: Interface Design). Then run it and see if you got it right.
+using ThinkJulia
 
 function draw(t, length, n)
     if n == 0
@@ -17,5 +18,12 @@ function draw(t, length, n)
 end
 
 println("Ans: ")
+
+# Ans: It should recursive pattern
+turtle = Turtle()
+
+@svg begin
+    draw(turtle, 10, 5)
+end
 
 println("End.")
