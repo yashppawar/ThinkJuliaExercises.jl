9a39246e2117e63607ed41803f9791ab5ba0adbd
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-24 05:40:11 +0000

    🎁 complete ch6/ex3

--- Chapter6/ex3.jl
+++ Chapter6/ex3.jl
@@ -2,4 +2,10 @@
 ## Write a function isbetween(x, y, z) that returns true if x ≤ y ≤ z or false otherwise.
 println("Ans: ")
 
+function isBetween(x, y, z)
+    return x ≤ y ≤ z
+end
+
+println(isBetween(1, 2, 3))
+
 println("End.")
