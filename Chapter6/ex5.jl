a5c73c879b140b59fcf2d393833dec0c29129c92
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-24 05:54:11 +0000

    ➕ add ch6/ex5

---
+++ Chapter6/ex5.jl
@@ -0,0 +1,11 @@
+## Exercise 6-5
+## The Ackermann function, A(m,n), is defined:
+
+##           ⎧ n + 1  if m = 0
+## A(m, n) = ⎨ A(m-1, 1) if m>0 &n = 0
+##           ⎩ A(m-1, A(m, n-1)) if m>0 and n>0.  
+
+## See https://en.wikipedia.org/wiki/Ackermann_function. Write a function named ack that evaluates the Ackermann function. Use your function to evaluate ack(3, 4), which should be 125. What happens for larger values of m and n?
+println("Ans: ")
+
+println("End.")
