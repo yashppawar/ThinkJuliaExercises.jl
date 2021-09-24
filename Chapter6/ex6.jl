56d6010e4c254238776159e844ac1073b1d40072
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-09-24 06:13:47 +0000

    🎁 complete ch6/ex6

--- Chapter6/ex6.jl
+++ Chapter6/ex6.jl
@@ -24,7 +24,31 @@ end
 ## 1. Test these functions out. What happens if you call middle with a string with two letters? One letter? What about the empty string, which is written "" and contains no letters?
 println("Ans 1: ")
 
+println(first("first")) # returns 'f'
+println(last("last")) # returns 't'
+println(middle("middle")) # returns "iddl"
+
+println(middle("as")) # returns ''
+println(middle("A")) # returns ''
+# println(middle("")) # error, empty string
+
 ## 2. Write a function called ispalindrome that takes a string argument and returns true if it is a palindrome and false otherwise. Remember that you can use the built-in function length to check the length of a string.
 println("Ans 2: ")
+
+function ispalindrome(word)
+    if length(word) == 0
+        return true
+    end
+
+    if first(word) == last(word)
+        return ispalindrome(middle(word))
+    else
+        return false
+    end
+end
+
+println(ispalindrome("noon"))
+println(ispalindrome("redivider"))
+println(ispalindrome("name"))
 
 println("End.")
