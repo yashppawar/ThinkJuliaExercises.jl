021a7e088caad917c404e1413274c746836ab189
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-12-08 02:29:30 +0000

    banana, ex8

---
+++ Chapter8/ex8.jl
@@ -0,0 +1,11 @@
+## Exercise 8-8
+## There is a built-in function called count that is similar to the function in Looping and Counting. Read the documentation of this function and use it to count the number of a’s in "banana".
+println("Ans: ")
+
+println("Please enter something: ")
+input = strip(readline()) 
+
+println(count("a", input))
+
+println("End.")
+ 
