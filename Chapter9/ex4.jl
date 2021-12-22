34609c52d6e71234c4170262349bbdc1af683b15
Yash Pawar <72196156+yashppawar@users.noreply.github.com>
2021-12-22 02:55:01 +0000

    🎉 complete ex5 of ch9

---
+++ Chapter9/ex4.jl
@@ -0,0 +1,20 @@
+## Exercise 9-4
+# Write a function named usesonly that takes a word and a string of letters, and that returns true if the word contains only letters in the list. Can you make a sentence using only the letters acefhlo? Other than "Hoe alfalfa?"
+
+println("Ans:")
+
+function usesonly(word, letters)
+    letters *= " "
+
+    for letter in lowercase(word)
+        if letter ∉ letters
+            return false
+        end
+    end
+    
+    return true
+end
+
+println(usesonly("Hoe alfalfa", "acefhlo"))
+
+println("End.")
